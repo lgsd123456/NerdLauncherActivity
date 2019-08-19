2aef5031a1d6ef95a434af6fbcf5818edd05cc5c
lgsd123456 <hbhg8612814@hotmail.com>
2019-08-19 19:00:47 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/pax/lg/nerdlauncheractivity/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.com.pax.lg.nerdlauncheractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+}
